e8c627c8214d61b63e192985110bb2be5fceb924
Deniss <denmantm@inbox.lv>
2016-10-11 19:59:39 +0000

    ch

--- views/main.php
+++ views/main.php
@@ -38,6 +38,34 @@
     }
     
     scrollIntoView('#today','#calendar')
+    
+    
+    
+    $.fn.followTo = function (pos) {
+    var $this = this,
+        $window = $(window);
+
+    $window.scroll(function (e) {
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('#calendar').followTo(250);
+    
+    
+    
+    
+    
   </script>
 </head>
 
