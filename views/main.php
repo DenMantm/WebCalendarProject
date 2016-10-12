a12c100ce75493128b7cd084a280c5a371b13baa
kamillasecki <kamil.lasecki@eircom.net>
2016-10-12 11:19:49 +0000

    Added the new meeting form

--- views/main.php
+++ views/main.php
@@ -10,10 +10,15 @@
   <!--Bootstrap-->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!--Date picker-->
+  <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
+  <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+  <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
   <!--Styling for Metro: responsible for floading notes menus etc-->
   <link rel="stylesheet" href="/css/metro.min.css">
   <!--Metro icons used in lefthandside menu-->
   <link rel="stylesheet" href="/css/metro-icons.min.css">
+  <link rel="stylesheet" href="/css/calendar.css">
   <!--Metro js-->
   <script type='text/javascript' src="/js/metro.min.js"></script>
   <!--Knockout used to ling input and output in realtime-->
@@ -21,98 +26,92 @@
   <script type='text/javascript' src="/js/bindings.js"></script>
   <!--Dragscroll used in calendar allowing scroll items by dragging them-->
   <script type="text/javascript" src="https://cdn.rawgit.com/asvd/dragscroll/master/dragscroll.js"></script>
-  <!--adding jquerry-->
-  <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>-->
   <script>
-  /*global $*/
+    /*global $*/
     function scrollIntoView(element, container) {
-      var containerTop = $(container).scrollTop(); 
-      var containerBottom = containerTop + $(container).height(); 
-      var elemTop = element.offsetTop;
-      var elemBottom = elemTop + $(element).height(); 
+      var containerTop = $(container).scrollTop();
+      var containerBottom = containerTop + $(container).height();
+      var elemTop = element.get(0).offsetTop;
+      var elemBottom = elemTop + $(element).height();
       if (elemTop < containerTop) {
         $(container).scrollTop(elemTop);
-      } else if (elemBottom > containerBottom) {
+      }
+      else if (elemBottom > containerBottom) {
         $(container).scrollTop(elemBottom - $(container).height());
       }
     }
-    
-    scrollIntoView('#today','#calendar')
+
+    scrollIntoView('#today', '#calendar')
   </script>
 </head>
 
 <body>
-<?php
+  <?php
 require('partials/navbar.php');
 ?>
-  
-  <!--Top menu holder temporary used for testing knockout-->
-  <div class="row">
-    <div class="col-md-11">
-      <input data-bind="value: firstName" />
-      <span data-bind="text: firstName"></span>
-    </div>
-  </div>
 
-  <div class="row">
-    
-    <!--##################-->
-    <!--Left handside menu-->
-    <!--##################-->
-    
-    <div class="col-md-1">
-      <div class="cell">
-        <ul class="sidebar no-responsive-future" id="sidebar">
-          <li>
-            <a href="#">
-              <span class="mif-calendar icon"></span>
-              <span class="title">Calendar</span>
-              <span class="counter">0</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="mif-event-available icon"></span>
-              <span class="title">Meetings</span>
-              <span class="counter">0</span>
-            </a>
-          </li>
-          <li class="active">
-            <a href="#">
-              <span class="mif-list icon"></span>
-              <span class="title">Tasks</span>
-              <span class="counter">0</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="mif-file-text icon"></span>
-              <span class="title">Notes</span>
-              <span class="counter">0</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="mif-cogs icon"></span>
-              <span class="title">Settings</span>
-              <span class="counter">0</span>
-            </a>
-          </li>
-        </ul>
+
+    <div class="row">
+
+      <!--##################-->
+      <!--Left handside menu-->
+      <!--##################-->
+
+      <div class="col-md-1 remove_right_padding dark" style="height:500px;">
+
+        </style>
+        <div class="cell">
+          <ul class="sidebar no-responsive-future" id="sidebar">
+            <li>
+              <a href="#">
+                <span class="mif-calendar icon"></span>
+                <span class="title">Calendar</span>
+                <span class="counter">0</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="mif-event-available icon"></span>
+                <span class="title">Meetings</span>
+                <span class="counter">0</span>
+              </a>
+            </li>
+            <li class="active">
+              <a href="#">
+                <span class="mif-list icon"></span>
+                <span class="title">Tasks</span>
+                <span class="counter">0</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="mif-file-text icon"></span>
+                <span class="title">Notes</span>
+                <span class="counter">0</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="mif-cogs icon"></span>
+                <span class="title">Settings</span>
+                <span class="counter">0</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
 
-    <!--##################-->
-    <!--###  Calendar ####-->
-    <!--##################-->
-    
-      <div class="col-md-3">
-        <div id="calendar" class="dragscroll" style="height: 500px; overflow: scroll; cursor: grab; cursor : -o-grab; cursor : -moz-grab; cursor : -webkit-grab;">
-        <table class="border bordered striped hovered cell-hovered table">
-          
+      <!--##################-->
+      <!--###  Calendar ####-->
+      <!--##################-->
+
+      <div class="col-md-3 remove_padding">
+        <div id="calendar" class="dragscroll borderless_cal" style="height: 500px;">
+          <table class="border bordered striped hovered cell-hovered table remove_margin">
 
 
-          <?php
+
+            <?php
     
           for($i=-20 ; $i<20 ; $i++){
              $date = date("d", time() + 60 * 60 * 24 * $i) ;
@@ -127,57 +126,63 @@ require('partials/navbar.php');
              }
           }
           ?>
-        </table>
+          </table>
         </div>
       </div>
 
-    <!--#########################-->
-    <!--###  Daily drilldown ####-->
-    <!--#########################-->
+      <!--#########################-->
+      <!--###  Daily drilldown ####-->
+      <!--#########################-->
 
-      <div class="col-md-3">
+      <div class="col-md-3 remove_padding">
         <!--Wrap div to allow draggable scroll TODO: size of the window will have to scale to the size of the browser-->
-         <div class="dragscroll" style="height: 500px; overflow: scroll; cursor: grab; cursor : -o-grab; cursor : -moz-grab; cursor : -webkit-grab;">
-           
+        <div class="dragscroll borderless_cal" style="height: 500px;">
+
           <!--Table itself.-->
-        <table class="border bordered striped hovered cell-hovered table">
-                    <?php
+          <table class="border bordered striped hovered cell-hovered table remove_margin">
+            <?php
     
           for($i=-12 ; $i<12 ; $i++){
              $hour = date("H", time() + 60 * 60 * $i) ;
-             $weekday = date("D", time() + 60 * 60 * 24 * $i);
-             $month = date("M", time() + 60 * 60 * 24 * $i);
-
-              echo("<tr><td>".$hour.":00</td><td></td></tr>");
+              if ($hour == date("H")){
+                echo("<tr><td><b>".$hour.":00</b></td><td></td></tr>");
+              } else {
+                echo("<tr><td>".$hour.":00</td><td></td></tr>");
+              }
+              
+              
              
           }
           ?>
-        
-          <tr>
-            <td>
-              08:00
-            </td>
-            <td onclick="$('#dialog').data('dialog').toggle()">Meeting
-            </td>
-          </tr>
-        </table>
+
+              <tr>
+                <td>
+                  08:00
+                </td>
+                <td onclick="$('#dialog').data('dialog').toggle()">Meeting
+                </td>
+              </tr>
+          </table>
         </div>
       </div>
-      
+
       <!--#################################-->
       <!--### Div with meeting details ####-->
       <!--#################################-->
-      
-       <div id="dialog" data-role="dialog,draggable" class="dialog container" data-close-button="true" style="width: auto; height: auto; visibility: visible; left: 427px; top: 320.5px; cursor: auto; z-index: 1050;">
-        <h1>Meeting with famous hitman</h1>
-            <p>Discuss the details of the kidnapping and killing various people as and day to day exercise.</p>
-    </div>
 
-    <!--####################-->
-    <!--###  Notes area ####-->
-    <!--####################-->
+      <div id="dialog" data-role="dialog,draggable" class="dialog container" data-close-button="true" style="width: auto; height: auto; visibility: visible; left: 427px; top: 320.5px; cursor: auto; z-index: 1050;">
+        <h1>Meeting with famous hitman</h1>
+        <p>Discuss the details of the kidnapping and killing various people as and day to day exercise.</p>
+
+      </div>
+
+      <!--####################-->
+      <!--###  Notes area ####-->
+      <!--####################-->
 
       <div class="col-md-4">
+        <button type="button" id="newmeetingBtn" class="btn btn-default btn-lg btn-block" data-toggle="modal" data-target="#newmeeting">
+            New meeting</button>
         <div class="panel" style="width: 200px; z-index: auto; top: -2px; left: 50px;" data-role="draggable">
           <div class="heading">
             <span class="icon mif-file-text"></span>
@@ -207,13 +212,64 @@ require('partials/navbar.php');
         </div>
       </div>
     </div>
-    
+    <div class="row dark">
+      <div class="col-md-12 remove_right_padding dark" style="height:70px;"></div>
+      <div>
 
-  </div>
+      </div>
+
+      <!--Dialog box for new meeting-->
+
+      <div class="modal fade" id="newmeeting" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">New meeting</h4>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <label class="col-sm-2" control-label>Participants</label>
+                <div class="col-sm-10">
+                  <input type="text" id="m_to" class="form-control" placeholder="Enter participants here" required/>
+                </div>
+              </div>
+              <div class="row">
+                <label htmlFor="inputName" class="col-sm-2" control-label>Subject</label>
+                <div class="col-sm-10">
+                  <input type="text" id="m_subject" class="form-control" placeholder="Enter meeting subject here" required/>
+                </div>
+              </div>
+              <div class="row">
+                <label htmlFor="inputCode" class="col-sm-2" control-label>Location</label>
+                <div class="col-sm-10">
+                  <input type="text" id="m_location" class="form-control" placeholder="Enter meeting location here" required/>
+                </div>
+              </div>
+              <div class="row">
+                <label htmlFor="inputDate" class="col-sm-2" control-label>Date</label>
+                <div class="col-sm-10">
+                  <input type="text" id="newDate" class="form-control" placeholder="Enter date" required/>
+                  <script type="text/javascript">
+                    $(function() {
+                      $('#newDate').datetimepicker();
+                    });
+                  </script>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button id="btnSaveNewMeeting" class="btn btn-primary">Save fund</button>
+            </div>
+
+          </div>
+        </div>
 
 
 
-   
+
 
 
 
