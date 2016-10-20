6f93c27ef1cce287401232debd9e2e6772de7e00
Deniss <denmantm@inbox.lv>
2016-10-20 18:34:27 +0000

    change, adding to partials

--- views/main.php
+++ views/main.php
@@ -3,32 +3,7 @@
 
 <head>
   <title>Main</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--Jquery-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <!--Bootstrap-->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <!--Date picker-->
-  <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
-  <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-  <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
-  <!--Styling for Metro: responsible for floading notes menus etc-->
-  <link rel="stylesheet" href="/css/metro.min.css">
-  <!--Metro icons used in lefthandside menu-->
-  <link rel="stylesheet" href="/css/metro-icons.min.css">
-  <link rel="stylesheet" href="/css/calendar.css">
-  <!--Metro js-->
-  <script type='text/javascript' src="/js/metro.min.js"></script>
-  <!--Knockout used to ling input and output in realtime-->
-  <script type='text/javascript' src='/js/knockout-3.4.0.debug.js'></script>
-  <script type='text/javascript' src="/js/bindings.js"></script>
-  <!--Dragscroll used in calendar allowing scroll items by dragging them-->
-  <script type="text/javascript" src="https://cdn.rawgit.com/asvd/dragscroll/master/dragscroll.js"></script>
-  
-  <!--this script is for the database exchange ajax/jquery-->
-  <script type='text/javascript' src="/js/database.js"></script>
+<?php include('partials/head.php') ?>
   <script>
     /*global $*/
 
