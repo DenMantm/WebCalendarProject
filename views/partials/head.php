ce2a4f0329e122cef4a8bc064fe138dc56cd3c07
kamillasecki <kamil.lasecki@eircom.net>
2016-11-22 19:07:13 +0000

    Updates to head

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,44 +1,9 @@
-
-<!--  <meta charset="utf-8">-->
-<!--  <meta name="viewport" content="width=device-width, initial-scale=1">-->
-  <!--Jquery-->
-<!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
-<!--  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">-->
-<!--  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>-->
-  <!--Bootstrap-->
-<!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-<!--  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-  <!--Date picker-->
-<!--  <script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>-->
-<!--  <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>-->
-  
-<!--  <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />-->
-  <!--Styling for Metro: responsible for floading notes menus etc-->
-<!--  <link rel="stylesheet" href="/css/metro.min.css">-->
-<!--  <link rel="stylesheet" href="/css/animate.css">-->
-  <!--Metro icons used in lefthandside menu-->
-<!--  <link rel="stylesheet" href="/css/metro-icons.min.css">-->
-<!--  <link rel="stylesheet" href="/css/calendar.css">-->
-  <!--Metro js-->
-<!--  <script type='text/javascript' src="/js/metro.min.js"></script>-->
-  <!--Dragscroll used in calendar allowing scroll items by dragging them-->
-<!--  <script type="text/javascript" src="https://cdn.rawgit.com/asvd/dragscroll/master/dragscroll.js"></script>-->
-  
-  <!--this script is for the database exchange ajax/jquery-->
-<!--  <script type='text/javascript' src="/js/database.js"></script>-->
-  
-  
-<!--  <link href='calendarLib/fullcalendar.css' rel='stylesheet' />-->
-<!--<link href='calendarLib/fullcalendar.print.css' rel='stylesheet' media='print' />-->
-<!--<link href='css/main.css' rel='stylesheet' media='print' />-->
-<!--<script src='calendarLib/lib/moment.min.js'></script>-->
-<!--<script src='calendarLib/fullcalendar.min.js'></script>-->
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>-->
-
  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!--Jquery-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <!--Bootstrap-->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -55,8 +20,7 @@
   <link rel="stylesheet" href="/css/calendar.css">
   <!--Metro js-->
   <script type='text/javascript' src="/js/metro.min.js"></script>
-  <!--Knockout used to ling input and output in realtime-->
-
+  <!--Dragscroll used in calendar allowing scroll items by dragging them-->
   <script type="text/javascript" src="https://cdn.rawgit.com/asvd/dragscroll/master/dragscroll.js"></script>
   
   <!--this script is for the database exchange ajax/jquery-->
@@ -67,16 +31,5 @@
 <link href='calendarLib/fullcalendar.print.css' rel='stylesheet' media='print' />
 <link href='css/main.css' rel='stylesheet' media='print' />
 <script src='calendarLib/lib/moment.min.js'></script>
-<script src='calendarLib/lib/jquery.min.js'></script>
-<script src='calendarLib/lib/jquery-ui.min.js'></script>
 <script src='calendarLib/fullcalendar.min.js'></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-
-
-  <!--CUSTOM SCRIPTS FOR MAIN-->
-  <script type="text/javascript" src="js/target.js"></script>
-  <script type="text/javascript" src="js/main.js"></script>
-<!--<script type="text/javascript" src="js/npm.js"></script>-->
-<!--<script type="text/javascript" src="js/bootstrap.js"></script>-->
-<!--<script type="text/javascript" src="js/ bootbox.min.js"></script>-->
-  
