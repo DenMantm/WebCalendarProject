5e1877afd80bcf82e1b7b27b2d803565b7b0ee23
arezki <a.ibsaine@hotmail.fr>
2016-11-22 20:28:27 +0000

    Adding the crud functionalities

--- views/check.php
+++ views/check.php
@@ -17,7 +17,7 @@ include_once("../modules/db.php");
 
 <?php include('partials/head.php') ?>
  
-            
+            <script src="js/check.js"></script>           
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>  
@@ -104,4 +104,4 @@ include('partials/navbar.php');
  </html>  
  
   
-<script src="js/check.js"></script>
+
