c6ae8edec4d5dcbc8b614be7fef279e2a85dfa90
arezki <a.ibsaine@hotmail.fr>
2016-11-27 14:48:21 +0000

    pulling

--- views/search/ajax/delete.php
+++ views/search/ajax/delete.php
@@ -2,7 +2,7 @@
 if (isset($_POST['id']) && isset($_POST['id']) != "") {
    include_once('../views/search/ajax/lib.php');
     $user_id = $_POST['id'];
- echo " helo";
+ 
     $object = new CRUD();
     $object->Delete($user_id);
 }
