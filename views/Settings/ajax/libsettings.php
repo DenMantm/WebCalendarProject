426ef12f0709b065d2fafc4f331e457273a1bc0f
arezki <a.ibsaine@hotmail.fr>
2016-12-05 14:13:22 +0000

    new form for user settings

--- views/Settings/ajax/libsettings.php
+++ views/Settings/ajax/libsettings.php
@@ -19,13 +19,15 @@ class CRUD
     }
  
    
-    public function Update($Email, $Username,$Password,$Salt)
+    public function Update($Email,$Firstname,$Surname,$Username,$Password,$Salt)
     {
-        $query = $this->db->prepare("UPDATE Users  SET  email = :Email,username=:Username, password=:Password, salt=:Salt where username= :Name");
+        $query = $this->db->prepare("UPDATE Users  SET  email = :Email,name=:Firstname,surname=:Surname,username=:Username, password=:Password, salt=:Salt where username= :Name");
        // $name=$_SESSION['user']['username'];
         $query->bindParam("Email", $Email, PDO::PARAM_STR);
         $query->bindParam("Name", $_SESSION['user']['username'], PDO::PARAM_STR);
         $query->bindParam("Username", $Username, PDO::PARAM_STR);
+         $query->bindParam("Firstname", $Firstname, PDO::PARAM_STR);
+          $query->bindParam("Surname", $Surname, PDO::PARAM_STR);
         $query->bindParam("Password", $Password, PDO::PARAM_STR);
         $query->bindParam("Salt", $Salt, PDO::PARAM_STR);
          
@@ -36,5 +38,7 @@ class CRUD
     
  
 }
+
+
  
 ?>
